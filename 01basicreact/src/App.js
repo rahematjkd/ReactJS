ba3de405cdc7b7bbac66b4ab0370160ad4d9488d
rahematjkd <rahematjkd@gmail.com>
2025-09-27 16:00:47 +0530

    Basic react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Chai aur react | Rahemat Jamakhandi</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Chai aur react with vite | Rahemat J</h1>
+  )
+}
+
+export default App
