719d1e221d59bbb9541ca051b9b97b8b714f3b7f
rahematjkd <rahematjkd@gmail.com>
2025-09-29 21:33:29 +0530

    React project 9: Context API with theme switcher project

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust paths to your project's files
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
